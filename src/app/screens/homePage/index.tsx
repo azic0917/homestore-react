b354559da4e6966d489d6dfbe2aeb914e8a9c598
frank <azic17092000@gmail.com>
2026-08-28 05:07:49 +0900

    BRR: Initial Setup for HomeStore React App modification

---
+++ src/app/screens/homePage/index.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
+import Events from "./Events";
+import NewDishes from "./NewDishes";
+import PopularDishes from "./PopularDishes";
+import Statistics from "./Statistics";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
+import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
+
+/** REDUX SLICE & SELECTOR **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
+});
+
+export default function HomePage() {
+  const { setPopularDishes, setNewDishes, setTopUsers } =
+    actionDispatch(useDispatch());
+
+  useEffect(() => {
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => setPopularDishes(data))
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+      })
+      .then((data) => setNewDishes(data))
+      .catch((err) => console.log(err));
+
+    const member = new MemberService();
+    member
+      .getTopUsers()
+      .then((data) => setTopUsers(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
+}
